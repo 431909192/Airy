467305a7e908826263a3b4e5cb26709ee69cfe35
431909192 <431909192@qq.com>
2026-06-10 19:38:10 +0800

    Delete Airy directory

--- Airy/app/src/main/java/com/mazhuo/airy/AiryApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.mazhuo.airy
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class AiryApplication : Application()
--- Airy/app/src/main/java/com/mazhuo/airy/domain/model/ProtocolType.kt
+++
@@ -1,5 +0,0 @@
-package com.mazhuo.airy.domain.model
-
-enum class ProtocolType {
-    FTP, SFTP, FTPS, SMB, WEBDAV
-}
